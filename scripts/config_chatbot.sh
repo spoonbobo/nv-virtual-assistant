7da193c6e77bcc92c334847f1dedfc9c3d6c2ae0
Season <73148791+spoonbobo@users.noreply.github.com>
2022-04-28 20:07:46 +0800

    Update config_chatbot.sh
    Add domain field

--- scripts/config_chatbot.sh
+++ scripts/config_chatbot.sh
@@ -1,6 +1,7 @@
 RIVA_MOUNTED_DATA_DIR='/data'
 RIVA_MOUNTED_SPECS_DIR='/specs'
 RIVA_MOUNTED_RESULTS_DIR='/results'
+DOMAIN=<your domain i.e. field of focus for the queries>
 LOCAL_RESULT_DIR=<local results folder>
 RIVA_REPO="path/to/riva_quickstart_v1.10.0-beta"
 RIVA_SERVICE_MAKER=nvcr.io/nvidia/riva/riva-speech:1.10.0-beta-servicemaker
